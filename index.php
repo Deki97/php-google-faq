3bc7b8412743aa890d2c12c25085a6bc02124f47
Deki97 <dejan97@libero.it>
2022-01-25 15:15:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <!-- Riscrivere questa pagina del sito google
+    https://policies.google.com/faq
+    Ci sono diverse domande con relative risposte.
+    Creare il “Database” (struttura dati) e la visualizzazione di queste domande e risposte con PHP. -->
+
+    <!-- Header -->
+    <header>
+        <div class="header-top">
+            <div class="header-top-left">
+                <div class="logo">
+                    <a href="#">
+                        <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google Logo">
+                    </a>
+                </div>
+
+                <span class="privacy-terms">
+                    <a href="#">
+                        Privacy e termini
+                    </a>
+                </span>
+            </div>
+
+            <div class="header-top-right">
+                <div class="grid">
+                    <a href="#">
+                        <i class="fas fa-th"></i>
+                    </a>
+                </div>
+
+                <div class="user-icon">
+                    <a href="#">
+                        D
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="header-bottom">
+
+        </div>
+    </header>
+
+    <!-- Main -->
+    <main>
+
+    </main>
+
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+
+</body>
+</html>
